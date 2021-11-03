639673c3b182b806165509f83ae874638915fe3e
sravanikodipelly1 <S544914@NWMISSOURI.EDU>
2021-11-03 17:36:23 -0500

    part-6 committed

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ManagerRouter = require('./routes/Manager');
+var addmodsRouter = require('./routes/addmods');
+
 var app = express();
 
 // view engine setup
@@ -22,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Manager', ManagerRouter);
+app.use('/addmods', addmodsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
