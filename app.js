e6a1be0496a8c58cfa073d68672f252a2ff2d00d
sravanikodipelly1 <S544914@NWMISSOURI.EDU>
2021-11-03 18:13:22 -0500

    part-8 committed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ManagerRouter = require('./routes/Manager');
 var addmodsRouter = require('./routes/addmods');
-
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Manager', ManagerRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
